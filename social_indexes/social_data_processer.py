70c43a244f455b4bde628878c1334f50010202d1
admire <admire@kartoza.com>
2020-11-04 12:51:57 +0200

    Modify scripts and formulae

--- social_indexes/social_data_processer.py
+++ social_indexes/social_data_processer.py
@@ -13,8 +13,8 @@ target = r'%s' % root_directory
 # Download stata data from https://www.datafirst.uct.ac.za/dataportal/index.php
 for stata_data in os.listdir(target):
     if stata_data.endswith('.dta'):
-        csv_output = stata_data + '.csv'
-        stata_lyr = stata_data + '.dta'
-
-        data = pd.io.stata.read_stata('%s') % stata_lyr
-        data.to_csv('%s') % csv_output
+        file_name = os.path.splitext(stata_data)[0]
+        csv_output = os.path.join(target, file_name + '.csv')
+        stata_lyr = os.path.join(target, stata_data)
+        data = pd.io.stata.read_stata(stata_lyr)
+        data.to_csv(csv_output)
--- social_indexes/social_indexes.sql
+++ social_indexes/social_indexes.sql
@@ -171,23 +171,59 @@ CREATE MATERIALIZED VIEW public.mv_direct_supply_of_water_from_the_environment_i
    FROM sample
 WITH DATA;
 
+-- Create an index based on the water access survey villages
+CREATE MATERIALIZED VIEW mv_water_access_by_village_index AS
+with sample as (
+SELECT id, st_transform(geom,32735) as geom, village,
+sum (  borehole  + "natural spring" + "tanker truck" + "river/stream"+ "dug well"  ) / st_area(st_transform(geom,32735)) * 1000000 as water_density
+	FROM public.wateraccess_by_village
+group by (id,village,geom)
+	)
+	SELECT sample.id,
+    sample.village,
+        CASE
+            WHEN (10::double precision * (sample.water_density / (( SELECT percentile_cont(0.9::double precision)
+                WITHIN GROUP (ORDER BY sample_1.water_density) AS percentile_cont
+               FROM sample sample_1)))) > 10::double precision THEN 10::double precision
+            ELSE 10::double precision * (sample.water_density / (( SELECT percentile_cont(0.9::double precision)
+                WITHIN GROUP (ORDER BY sample_1.water_density) AS percentile_cont
+               FROM sample sample_1)))
+        END AS index,
+    sample.geom
+   FROM sample;
+
+
 -- Poverty Index Calculations
 -- Create a materialized view dependency ratio . This is a join between the table "employment-status-hhold-head", sub_place
 -- and wbr aoi.
 CREATE MATERIALIZED VIEW public.mv_dependency_ratio
- AS
+TABLESPACE pg_default
+AS
  WITH limpopo_subplace AS (
-         SELECT a_1.*,
+         SELECT a_1.id,
+            a_1.sp_code,
+            a_1.sp_name,
+            a_1.employed,
+            a_1.unemployed,
+            a_1.dosicoraged_worker_seeker,
+            a_1.not_economically_active,
+            a_1.less_than_15,
             b_1.geom
-           FROM "employment-status-hhold-head" a_1
-             JOIN sub_place b_1 ON a_1.sp_code = b_1.sp_code
-
+           FROM ("employment-status-hhold-head" a_1
+             JOIN sub_place b_1 ON ((a_1.sp_code = b_1.sp_code)))
         )
- SELECT a.id,a.sp_code,a.sp_name, a.employed,
-a.unemployed, a.dosicoraged_worker_seeker, a.not_economically_active, a.less_than_15,
-    st_intersection(st_transform(a.geom,32735),st_transform(b.geom,32735)) AS geom
-   FROM limpopo_subplace a
-     JOIN aoi_wbr b ON st_intersects(b.geom, a.geom);
+ SELECT a.id,
+    a.sp_code,
+    a.sp_name,
+    a.employed,
+    a.unemployed,
+    a.dosicoraged_worker_seeker,
+    a.not_economically_active,
+    a.less_than_15,
+    st_intersection(st_transform(a.geom, 32735), st_transform(b.geom, 32735)) AS geom
+   FROM (limpopo_subplace a
+     JOIN aoi_wbr b ON (st_intersects(b.geom, a.geom)))
+WITH DATA;
 
 -- Create a layer with statistics for dependency ratio index
 
@@ -313,3 +349,94 @@ a.communal_refuse_dump, a.own_refuse_dump, a.no_refuse_dump, a.other, a.unspecif
 
 --TODO
 -- Add mv_access_to_services_refuse_disposal_index below here
+
+
+-- Consumption ( Level of ownership for various goods as proxy to poverty)
+-- Create table for combinining all the indicatiors
+
+CREATE MATERIALIZED VIEW mv_level_of_ownership
+
+AS
+ SELECT a.fid,
+    a.sp_code,
+    a.sp_name,
+    a.cell_1,
+    a.cell_2,
+    a.comp_1,
+    a.comp_2,
+    a.dvdply_1,
+    a.dvdply_2,
+    a.elgstv_1,
+    a.elgstv_2,
+    a.mtrcar_1,
+    a.mtrcar_2,
+    a.radio_1,
+    a.radio_2,
+    a.refrig_1,
+    a.refrig_2,
+    a.sattv_1,
+    a.sattv_2,
+    a.tv_1,
+    a.tv_2,
+    a.vcmcln_1,
+    a.vcmcln_2,
+    a.wshmch_1,
+    a.wshmch_2,
+    b.geom
+   FROM ownership_proxy a
+     JOIN sub_place b ON b.sp_code::character varying::text = a.sp_code::text
+WITH DATA;
+
+-- Create level of ownership clipped to the WBR boundary
+CREATE MATERIALIZED VIEW mv_level_of_ownership_wbr as
+SELECT a.fid, a.sp_code, a.sp_name, a.cell_1, a.cell_2, a.comp_1, a.comp_2, a.dvdply_1, a.dvdply_2, a.elgstv_1, a.elgstv_2,
+     a.mtrcar_1, a.mtrcar_2, a.radio_1, a.radio_2, a.refrig_1, a.refrig_2, a.sattv_1, a.sattv_2, a.tv_1,
+	 a.tv_2, a.vcmcln_1, a.vcmcln_2, a.wshmch_1, a.wshmch_2,
+       st_intersection(st_transform(a.geom,32735),st_transform(b.geom,32735)) AS geom
+   FROM mv_level_of_ownership a
+     JOIN aoi_wbr b ON st_intersects(b.geom, a.geom);
+
+-- Create index for consumption
+CREATE MATERIALIZED VIEW mv_level_of_ownership_index AS
+with sample as (
+SELECT fid, sp_code, sp_name,
+round(1 - (sum (cell_1::numeric + comp_1::numeric + dvdply_1::numeric + elgstv_1::numeric
+	 + mtrcar_1::numeric + radio_1::numeric + refrig_1::numeric + sattv_1::numeric + tv_1::numeric + vcmcln_1::numeric + wshmch_1::numeric ) / (
+	sum (cell_1::numeric + comp_1::numeric + dvdply_1::numeric + elgstv_1::numeric + mtrcar_1::numeric
+		 + radio_1::numeric + refrig_1::numeric + sattv_1::numeric + tv_1::numeric + vcmcln_1::numeric + wshmch_1::numeric ) +
+	sum (cell_2::numeric +  comp_2::numeric +  dvdply_2::numeric +  elgstv_2::numeric +
+ mtrcar_2::numeric +  radio_2::numeric +  refrig_2::numeric +
+sattv_2::numeric +  tv_2::numeric +  vcmcln_2::numeric + wshmch_2::numeric )))::numeric,4)  / st_area(geom) * 1000000 as ownership_ratio ,
+
+geom
+	FROM public.mv_level_of_ownership_wbr
+	group by (fid, sp_code,sp_name,geom)
+	)
+
+	SELECT sample.fid,
+    sample.sp_code,
+    sample.sp_name,
+    round(
+        CASE
+            WHEN (10::double precision * (sample.ownership_ratio / (( SELECT percentile_cont(0.9::double precision) WITHIN GROUP (ORDER BY sample_1.ownership_ratio) AS percentile_cont
+               FROM sample sample_1)))) > 10::double precision THEN round(10::numeric,3)
+            ELSE round(10::numeric * (sample.ownership_ratio / (( SELECT percentile_cont(0.9::double precision) WITHIN GROUP (ORDER BY sample_1.ownership_ratio) AS percentile_cont
+               FROM sample sample_1)))::numeric, 3)
+        END, 3) AS ownership_index,
+    sample.geom
+   FROM sample;
+
+-- Overall social index zonal stats
+CREATE MATERIALIZED VIEW mv_overal_social_index AS
+SELECT id,
+    round(
+        CASE
+            WHEN (100::double precision * mean /
+					(( SELECT percentile_cont(0.9::double precision) WITHIN GROUP (ORDER BY mean) AS percentile_cont
+               FROM quinary_catchment ))) > 100::double precision THEN 100::numeric
+            ELSE round((100::double precision * mean /
+					(( SELECT percentile_cont(0.9::double precision) WITHIN GROUP (ORDER BY mean) AS percentile_cont
+               FROM quinary_catchment )))::numeric,3)
+        END, 3) AS index,
+    geom
+   FROM quinary_catchment;
